62f5578fae85b310ac45a83593d860d63b9d9ed1
Sandy <icynxx@gmail.com>
2022-09-02 17:04:27 -0400

    basic frontend hello world

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import { render } from 'react-dom'; 
+import App from './App.js'
+import styles from './styles/styles.css'
+
+render(
+    <App />,
+    document.getElementById('root')
+);
