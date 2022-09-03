c3990108c5d960da154b3edd80dc869ffc169e33
Adam Selph <adam@Adams-MBP.fios-router.home>
2022-09-03 10:51:24 -0400

    add api route and SQL database query

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-// const apiRouter = require('./routes/api');
+const apiRouter = require('./routes/api');
 
 const PORT = 3000;
 
@@ -12,11 +12,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
+app.use('/api', apiRouter);
+
 app.get('/', (req, res) => {
   console.log('getting index.html')
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 })
 
 app.use((req, res) => res.status(404).send('Cannot get route'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(3000);
